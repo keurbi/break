2ed829219ac03bba62f652e3699fa33f800f9af6
Loïc <loic@example.com>
2025-04-23 14:32:12 +0200

    style réparé

--- break/tailwind.config.js
+++ break/tailwind.config.js
@@ -1,19 +1,20 @@
-// break/tailwind.config.js
+// tailwind.config.js
 module.exports = {
 	content: [
 	  './src/pages/**/*.{js,ts,jsx,tsx}',
 	  './src/components/**/*.{js,ts,jsx,tsx}',
+	  './src/app/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
 	  extend: {
 		colors: {
-		  "primary-color": "var(--primary-color)",
-		  "secondary-color": "var(--secondary-color)",
-		  "tertiary-color": "var(--tertiary-color)",
-		  "gray-custom": "var(--gray-custom)",
+		  primary: '#7346FF',
+		  secondary: '#CFAAFF',
+		  tertiary: '#D5DDF4',
+		  grayCustom: '#656565',
 		},
 	  },
 	},
-	plugins: [],
+	plugins: ["@tailwindcss/postcss"],
   };
   
